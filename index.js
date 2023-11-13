5a69c2ce43cead5e52c8df89663848000a292447
Ethan Hessler <ethanhessler@gmail.com>
2023-11-13 09:40:45 -0500

    initial docs

---
+++ index.js
@@ -0,0 +1,29 @@
+
+// const url = 'https://exercises-by-api-ninjas.p.rapidapi.com/v1/exercises?muscle=biceps';
+// const options = {
+// 	method: 'GET',
+// 	headers: {
+// 		'X-RapidAPI-Key': 'affff5fc7cmsh5968ae8730f2592p11fb2bjsn6c0de3b97452',
+// 		'X-RapidAPI-Host': 'exercises-by-api-ninjas.p.rapidapi.com'
+// 	}
+// };
+
+// try {
+// 	const response = await fetch(url, options);
+// 	const result = await response.text();
+// 	console.log(result);
+// } catch (error) {
+// 	console.error(error);
+// }
+
+fetch('https://exercises-by-api-ninjas.p.rapidapi.com/v1/exercises', {
+    method: 'GET',
+    params: {
+        difficulty: 'beginner',
+        offset: '40'
+      },
+	headers: {
+		'X-RapidAPI-Key': 'affff5fc7cmsh5968ae8730f2592p11fb2bjsn6c0de3b97452',
+		'X-RapidAPI-Host': 'exercises-by-api-ninjas.p.rapidapi.com'}
+}).then(resp => resp.json())
+.then(data => console.log(data))

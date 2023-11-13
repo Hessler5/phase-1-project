e42d5c48e3ad9f715d9d4b359261ae207b82c02d
Ethan Hessler <ethanhessler@gmail.com>
2023-11-13 10:17:27 -0500

    beginning javascript

--- index.js
+++ index.js
@@ -1,29 +1,47 @@
+document.addEventListener("DOMContentLoaded", () => {
 
-// const url = 'https://exercises-by-api-ninjas.p.rapidapi.com/v1/exercises?muscle=biceps';
-// const options = {
-// 	method: 'GET',
-// 	headers: {
-// 		'X-RapidAPI-Key': 'affff5fc7cmsh5968ae8730f2592p11fb2bjsn6c0de3b97452',
-// 		'X-RapidAPI-Host': 'exercises-by-api-ninjas.p.rapidapi.com'
-// 	}
-// };
+let submissionForm = document.querySelector("form")
+submissionForm.addEventListener("submit", () => {
 
-// try {
-// 	const response = await fetch(url, options);
-// 	const result = await response.text();
-// 	console.log(result);
-// } catch (error) {
-// 	console.error(error);
-// }
 
-fetch('https://exercises-by-api-ninjas.p.rapidapi.com/v1/exercises', {
+
+
+  
+fetch(`https://exercises-by-api-ninjas.p.rapidapi.com/v1/exercises?${type}${and1}${muscle}${and2}${difficulty}`, {
     method: 'GET',
-    params: {
-        difficulty: 'beginner',
-        offset: '40'
-      },
-	headers: {
-		'X-RapidAPI-Key': 'affff5fc7cmsh5968ae8730f2592p11fb2bjsn6c0de3b97452',
-		'X-RapidAPI-Host': 'exercises-by-api-ninjas.p.rapidapi.com'}
+	  headers: {
+		  'X-RapidAPI-Key': 'affff5fc7cmsh5968ae8730f2592p11fb2bjsn6c0de3b97452',
+	  	'X-RapidAPI-Host': 'exercises-by-api-ninjas.p.rapidapi.com'}
 }).then(resp => resp.json())
 .then(data => console.log(data))
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})

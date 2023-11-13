7f0f20ec80b9e4477411e2f3aedfb05d69d49538
Ethan Hessler <ethanhessler@gmail.com>
2023-11-13 11:38:53 -0500

    added search functioanlity

--- index.js
+++ index.js
@@ -1,16 +1,89 @@
 document.addEventListener("DOMContentLoaded", () => {
 
 let submissionForm = document.querySelector("form")
-submissionForm.addEventListener("submit", () => {
+submissionForm.addEventListener("submit", (e) => {
+e.preventDefault()
 
 
+//button values
+let exerciseDropDown = document.querySelector("#dropdown1")
+let muscleDropDown = document.querySelector("#dropdown2")
+let difficultyDropDown = document.querySelector("#dropdown3")
+
+let muscle;
+let type;
+let difficulty;
+let and1;
+let and2;
 
 
+function setButtonValue() {
+	if(exerciseDropDown.value != ""){
+		type = "type=" + exerciseDropDown.value;
+	} else { type = ""}
+	if(muscleDropDown.value != ""){
+		muscle = "muscle=" + muscleDropDown.value;
+	} else { muscle = ""}
+	if(difficultyDropDown.value != ""){
+		difficulty = "difficulty=" + difficultyDropDown.value;
+	} else { difficulty = ""}
+}
+
+
+setButtonValue()
+
+console.log(muscle)
+
+
+function andOne () {
+	if(muscle != "" && type != "") {
+		and1 = "&"
+	} else { and1 = "" }
+}
+
+function andTwo(){
+	if(difficulty != ""){
+		if(muscle != "" || type != ""){
+			and2 = "&"
+		} else { and2 = "" }
+	} else { and2 = "" }
+}
+
+andOne()
+andTwo()
+
+console.log(and1)
+console.log(and2)
   
-fetch(`https://exercises-by-api-ninjas.p.rapidapi.com/v1/exercises?${type}${and1}${muscle}${and2}${difficulty}`, {
+let fetchValue = `https://exercises-by-api-ninjas.p.rapidapi.com/v1/exercises?${type}${and1}${muscle}${and2}${difficulty}`
+
+
+fetch(fetchValue, {
     method: 'GET',
 	  headers: {
 		  'X-RapidAPI-Key': 'affff5fc7cmsh5968ae8730f2592p11fb2bjsn6c0de3b97452',
 	  	'X-RapidAPI-Host': 'exercises-by-api-ninjas.p.rapidapi.com'}
-}).then(resp => resp.json())
+}).then(resp => {
+	console.log(fetchValue)
+	return resp.json()
+})
 .then(data => console.log(data))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
+
+
+})

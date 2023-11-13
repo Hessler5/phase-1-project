ad3dcf7150543351c53a5d049ec8800e0710b321
Ethan Hessler <ethanhessler@gmail.com>
2023-11-13 12:25:12 -0500

    card functionality

--- index.js
+++ index.js
@@ -10,12 +10,15 @@ let exerciseDropDown = document.querySelector("#dropdown1")
 let muscleDropDown = document.querySelector("#dropdown2")
 let difficultyDropDown = document.querySelector("#dropdown3")
 
+//Search Variable
+
 let muscle;
 let type;
 let difficulty;
 let and1;
 let and2;
 
+//Populages search functionality based off of selections
 
 function setButtonValue() {
 	if(exerciseDropDown.value != ""){
@@ -29,11 +32,9 @@ function setButtonValue() {
 	} else { difficulty = ""}
 }
 
-
 setButtonValue()
 
-console.log(muscle)
-
+//adds and value if moer than one selection is made
 
 function andOne () {
 	if(muscle != "" && type != "") {
@@ -52,9 +53,12 @@ function andTwo(){
 andOne()
 andTwo()
 
-console.log(and1)
-console.log(and2)
-  
+//Array that gets pushed from the API
+
+let unsavedExercises = [];
+
+//fetch from API 
+
 let fetchValue = `https://exercises-by-api-ninjas.p.rapidapi.com/v1/exercises?${type}${and1}${muscle}${and2}${difficulty}`
 
 
@@ -63,12 +67,32 @@ fetch(fetchValue, {
 	  headers: {
 		  'X-RapidAPI-Key': 'affff5fc7cmsh5968ae8730f2592p11fb2bjsn6c0de3b97452',
 	  	'X-RapidAPI-Host': 'exercises-by-api-ninjas.p.rapidapi.com'}
-}).then(resp => {
-	console.log(fetchValue)
-	return resp.json()
-})
-.then(data => console.log(data))
+}).then(resp => {return resp.json()})
+.then(data => {
+	for(let i = 0; i < data.length; i++){
+		unsavedExercises.push(data[i])
+	}
+	console.log(unsavedExercises)
+	renderCards(unsavedExercises)
+	})
 
+//function to create cards at the top
+function renderCards(exerciseList){
+	let initialCardList = document.querySelector("#initial-exercises-list")
+	initialCardList.innerHTML = ""
+	for(index of exerciseList){
+		let newCardDiv = document.createElement("div")
+		newCardDiv.innerHTML = `
+		<h5>${index.name}</h5>
+		<p>${index.type}</p>
+		<p>${index.muscle}</p>
+		<p>${index.difficulty}</p>
+		<p>${index.equipment}</p>
+		<p>${index.instructions}</p>
+		`
+		initialCardList.appendChild(newCardDiv)
+	}
+}
 
 
 
